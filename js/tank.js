346f4205eecd671a89fb4ddbc979f7adb6239be9
leochiubowser <leochiubowser@gmail.com>
2022-08-27 14:25:42 +0800

    tank direction ver 1.0

--- js/tank.js
+++ js/tank.js
@@ -2,7 +2,10 @@ class Tank {
     constructor() {
         this.image = new Image();
         this.image.src = "../Image/Player1.png";
-        this.x = canvas.width / 2;
+        this.style = 0;
+        this.width = this.image.width / 8;
+        this.height = this.image.height / 4;
+        this.x = canvas.width / 2 - this.width / 2;
         this.y = canvas.height - 50;
         this.velocity = {
             x: 0,
@@ -11,20 +14,23 @@ class Tank {
         }
         this.direction = "up";
         this.scale = 1;
+        this.rotate = 0;
+        this.time = 0;
     }
     draw() {
 
         this.move();
         ctx.save();
-
+        ctx.translate(this.x + this.width / 2, this.y + this.height / 2);
+        ctx.rotate(this.rotate * Math.PI / 180);
         ctx.drawImage(
             this.image,
-            0,
-            -1,
+            this.style,
+            -0.5,
             this.image.width / 8,
             this.image.height / 4,
-            this.x - this.image.width / 16,
-            this.y - this.image.height / 16,
+            -this.width / 2,
+            -this.height / 2,
             this.image.width / 8,
             this.image.height / 4,
         );
@@ -40,19 +46,25 @@ class Tank {
             if (e.key == "ArrowUp" || e.key == "w" || e.key == "W") {
                 this.velocity.y = -this.velocity.speed;
                 this.direction = "up";
+                this.rotate = 0;
             }
             else if (e.key == "ArrowDown" || e.key == "s" || e.key == "S") {
                 this.velocity.y = this.velocity.speed;
                 this.direction = "down";
+                this.rotate = 180;
             }
             else if (e.key == "ArrowLeft" || e.key == "a" || e.key == "A") {
                 this.velocity.x = -this.velocity.speed;
                 this.direction = "left";
+                this.rotate = 270;
             }
             else if (e.key == "ArrowRight" || e.key == "d" || e.key == "D") {
                 this.velocity.x = this.velocity.speed;
                 this.direction = "right"
+                this.rotate = 90;
             }
+
+
         })
 
         addEventListener("keyup", () => {
@@ -60,6 +72,18 @@ class Tank {
             this.velocity.y = 0;
         })
 
+        this.time += 2;
+
+        if (this.velocity.x != 0 || this.velocity.y != 0) {
+            if (this.style == 0 && this.time > 10) {
+                this.style = 28;
+                this.time = 0;
+            }
+            else if (this.style == 28 && this.time > 10) {
+                this.style = 0;
+                this.time = 0;
+            }
+        }
 
         // Avoid X and Y direction happened at the same time 
 
